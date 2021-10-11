702d6201e2c18b7eefe6e4149bfeaf9bc679b9c0
Ali Karababa <karababa.alii@gmail.com>
2021-10-11 23:12:02 +0300

    In this app, I have tried to new properties about SwiftUI also, I have used to previous lesson information about UI design.

--- SwiftUI Slots/ContentView.swift
+++ SwiftUI Slots/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var credits: Int = 1025
+    @State private var firstImg: String = "apple"
+    @State private var secondImg: String = "cherry"
+    @State private var thirdImg: String = "star"
+    private var imgArray = ["apple", "star","cherry"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("SwiftUI Slots!")
+                .font(.largeTitle)
+            Spacer()
+            Text("Credits:" + String(credits))
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .foregroundColor(Color.red)
+            Spacer()
+            HStack(spacing: 20.0) {
+                Image(firstImg)
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                Image(secondImg)
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                Image(thirdImg)
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+            }
+            .padding(.horizontal, 20.0)
+            Spacer()
+            Button("Spin") {
+                self.credits -= 5
+              firstImg = imgArray.randomElement()!
+              secondImg = imgArray.randomElement()!
+              thirdImg = imgArray.randomElement()!
+                print("Credits: \(credits)")
+                if firstImg == secondImg && secondImg == thirdImg {
+                    self.credits += 50
+                }
+            }
+            .frame(width: 75.0, height: 35.0)
+            .foregroundColor(.white)
+            .background(Color.pink)
+            .cornerRadius(50)
+            Spacer()
+        }
     }
 }
 
