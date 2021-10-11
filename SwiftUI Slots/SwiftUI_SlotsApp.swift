74514e4a06600961ce23d462a41818b79cf9c515
Ali Karababa <karababa.alii@gmail.com>
2021-10-11 17:08:06 +0300

    Initial Commit

---
+++ SwiftUI Slots/SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SlotsApp.swift
+//  SwiftUI Slots
+//
+//  Created by Ali Karababa on 11.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
